836b0bebcfc7701c5c57049767cffc3d4fd6a9cc
Victor <victor.oliveira@dartdigital.com.br>
2025-09-12 17:21:51 -0300

    IOS

---
+++ composeApp/src/iosMain/kotlin/br/com/victorhgo/kotlinmovie/MainViewController.kt
@@ -0,0 +1,6 @@
+package br.com.victorhgo.kotlinmovie
+
+import androidx.compose.ui.window.ComposeUIViewController
+import platform.UIKit.UIViewController
+
+fun MainViewController(): UIViewController = ComposeUIViewController { App() }
